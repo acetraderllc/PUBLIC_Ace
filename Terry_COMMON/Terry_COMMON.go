bf6dfaa2515f8c60dab5e01a2b99aa182c24b9bf
Terry <terryosu@gmail.com>
2021-02-22 22:20:08 -0600

    a asdas

--- Terry_COMMON/Terry_COMMON.go
+++ Terry_COMMON/Terry_COMMON.go
@@ -207,6 +207,9 @@ func IS_EVEN(input_NUM int) bool {
 // Params: URL, UserAgent
 // Returns: bool, GOQUERY_DOC, Text of Response
 var DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+func SCRAPER(EXTRA_ARGS ...string) (bool, *goquery.Document, string) {
+	return SCRAPE_TOOL(EXTRA_ARGS...)
+}
 func SCRAPE_TOOL(EXTRA_ARGS ...string) (bool, *goquery.Document, string) {
 
 	C.Println("")
